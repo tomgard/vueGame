642b669c3e2187c2ef4aff1a7d43eb1de9434d82
tomgard <thomas.gardner@lntsoftware.com>
2018-11-21 15:02:25 +0000

    add ip method and get file data

--- process.php
+++ process.php
@@ -3,14 +3,31 @@
     $data = [];
     function getData()
     {
-        return file_get_contents (file).unserialize();
+        return file_get_contents (file).json_decode();
     }
     function saveData()
     {
-        file_put_contents(file, data.serialize());
+        file_put_contents(file, data.json_encode());
+    }
+    function getRealIpAddr()
+    {
+        if (!empty($_SERVER['HTTP_CLIENT_IP']))   //check ip from share internet
+        {
+          $ip=$_SERVER['HTTP_CLIENT_IP'];
+        }
+        elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))   //to check ip is pass from proxy
+        {
+          $ip=$_SERVER['HTTP_X_FORWARDED_FOR'];
+        }
+        else
+        {
+          $ip=$_SERVER['REMOTE_ADDR'];
+        }
+        return $ip;
     }
     if($_GET["x"] && $_GET["y"] && $_GET["direction"] && $_GET["sprite"]){
-        echo "all data present";
+        data = getData();
+        
     } else {
         echo "error";
     }
